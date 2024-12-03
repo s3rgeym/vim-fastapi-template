54028676716012f024766f24f2bb1fb2a3947c91
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-12-03 21:31:38 +0300

    Поехалиgit status

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def index():
+    return "Index Page."
+
+
+@app.get("/hello/{name}")
+async def read_item(name: str = "world"):
+    return {"message": f"Hello, {name}!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # В качестве app нужно указать строку для импорта
+    uvicorn.run("main:app", reload=True)
